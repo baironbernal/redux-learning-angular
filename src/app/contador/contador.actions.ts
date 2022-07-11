760d116a4757215664c52eaeb198ea08c446051b
Bairon Bernal <bairon.bernal@one-click.com.co>
2022-07-11 17:26:05 -0500

    init

---
+++ src/app/contador/contador.actions.ts
@@ -0,0 +1,4 @@
+import { createAction } from '@ngrx/store';
+
+export const incrementar = createAction('[Contador] Increementar');
+export const decrementar = createAction('[Contador] Decrementar');
